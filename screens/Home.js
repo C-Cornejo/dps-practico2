d105617d8c551520b0dee5fdf12847ee87096294
C-Cornejo <castillo.cornejo94@gmail.com>
2021-10-10 09:25:13 -0600

    Screen inicio completa

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import {Button, Text,View} from 'react-native';
+import {Button, Text,View,StyleSheet} from 'react-native';
 
  function Home(props)
 {
     const{navigation} = props; 
     return(
-        <View> 
-            <Text>Home: app desarrollada por Chris</Text>
+        <View  style={styles.nav}> 
+            <Text style={styles.etiqueta}>App desarrollada por</Text>
+            <Text style={styles.etiqueta}>Chris Cornejo</Text>
+            <Text style={styles.etiqueta}>Franklin Lovato</Text>
         </View>
     );
 }
+
+    const styles = StyleSheet.create({
+    nav:
+    {
+        padding:20,
+        alignContent:'center',
+   
+    },
+    etiqueta:
+    {
+        fontSize: 25
+    }
+});
 
 export default Home;
